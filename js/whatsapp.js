f51bfee724b9a1c373622f3f633ffc3b273fe57a
angelfilmaker <72461894+Angelfilmaker@users.noreply.github.com>
2024-04-07 14:10:46 -0300

    Delete js directory

--- js/whatsapp.js
+++
@@ -1,6 +0,0 @@
-function abrirChat() {
-    var numero = '1125835984'; // Reemplaza esto con el número de WhatsApp al que deseas enviar el mensaje
-    var mensaje = 'Hola, ¿cómo estás?'; // Puedes personalizar este mensaje si lo deseas
-    var url = 'https://wa.me/' + numero + '?text=' + encodeURIComponent(mensaje);
-    window.open(url, '_blank');
-}
