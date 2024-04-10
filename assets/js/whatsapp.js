4a5b7873f15425e1eccc3a2fc123d57eca0fe877
angelfilmaker <72461894+Angelfilmaker@users.noreply.github.com>
2024-04-10 14:57:39 -0300

    Update whatsapp.js

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,5 +1,5 @@
 function abrirChat() {
-    var numero = '+54 1125835984'; // Reemplaza esto con el número de WhatsApp al que deseas enviar el mensaje
+    var numero = '1125835984'; // Reemplaza esto con el número de WhatsApp al que deseas enviar el mensaje
     var mensaje = 'Hola, ¿cómo estás?'; // Puedes personalizar este mensaje si lo deseas
     var url = 'https://wa.me/' + numero + '?text=' + encodeURIComponent(mensaje);
     window.open(url, '_blank');
